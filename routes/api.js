3165849aca563e918de1d389c25c1ea8a9fef698
Ethan Shook <eshook89@gmail.com>
2020-12-06 18:25:20 -0600

    changed let to var

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+var data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
 module.exports = function(app) {
     app.get("/api/notes", function(req, res) {
@@ -9,14 +9,16 @@ module.exports = function(app) {
     app.get("/api/notes/:id", function(req, res) {
         res.json(data[Number(req.params.id)]);
     });
+
     app.post("/api/notes", function(req, res) {
+        
         let newNote = req.body;
         let uniqueId = (data.length).toString();
         console.log(uniqueId);
         newNote.id = uniqueId;
         data.push(newNote);
 
-        fs. writeFileSync("./db/db.json", JSON.stringify(data), function(err) {
+        fs.writeFileSync("./db/db.json", JSON.stringify(data), function(err) {
             if (err) throw (err);
         });
 
